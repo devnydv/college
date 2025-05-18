8c1fe61d778ca12114f22b88bc882621bb9186fe
devnydv <bittumailb2@gmail.com>
2025-05-18 18:55:44 +0530

    Refactor addstdn function to print form data; update student model to include address field

--- college/model.py
+++ college/model.py
@@ -9,3 +9,15 @@ class admin(db.Model):
 
     def to_dict(self):
         return {"id": self.id, "username": self.username, "password": self.password}
+    
+class student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    roll = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer, primary_key=True)
+    dob = db.Column(db.Integer, primary_key=True)
+    gender = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.Integer, primary_key=True)
+   
