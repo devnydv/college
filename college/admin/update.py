cc1ef7404745799166a0b54adfabbdc23db99493
devnydv <bittumailb2@gmail.com>
2025-06-12 22:48:45 +0530

    changes in updates

--- college/admin/update.py
+++ college/admin/update.py
@@ -8,6 +8,21 @@ designs = designations()
 
 @app.route("/admin/update/student/<id>", methods = ["GET", "POST"])
 def updatestdn(id):
+    if request.method == "POST":
+        data = request.form
+        student = Student.query.get(id)
+        student.name= data['name'],
+        student.roll= int(data['roll']),
+        student.email=data['email'],
+        student.phone= data['phone'],
+        student.semester= data['year'],
+        date = datetime.strptime(data['dob'], '%Y-%m-%d').date(),
+        student.dob= date
+        student.gender= data['gender'],
+        student.department= data['department'],
+        student.address= data['address']
+        db.session.commit()
+        return 'hmm'
     sem = semester()
     dep = departments()
     student = Student.query.get(id)
