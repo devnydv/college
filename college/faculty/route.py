4e4be462abab52891fd36331beef96e2ac25476c
devnydv <bittumailb2@gmail.com>
2025-07-19 00:31:24 +0530

    Add notices section to faculty and student profiles for improved communication

--- college/faculty/route.py
+++ college/faculty/route.py
@@ -1,14 +1,14 @@
 from college import app
 from flask import render_template, request, redirect, session, url_for
 from college.model import db, Faculty, Notice
-
+from sqlalchemy import desc
 @app.route("/facultyprofile/<id>", methods = ["GET", "POST"])
 def faculty_profile(id):
     if session.get('faculty') is None:
         return redirect(url_for("login"))
     else:
         faculty = Faculty.query.get(id)
-        
+        notices = Notice.query.order_by(desc(Notice.id)).limit(3).all()
         if not faculty:
             return "Faculty not found", 404
-        return render_template("faculty-profile.html", faculty=faculty, id=id, logged_in=True)
+        return render_template("faculty-profile.html", faculty=faculty, id=id, logged_in=True, notices=notices)
--- college/student/route.py
+++ college/student/route.py
@@ -1,6 +1,7 @@
 from college import app
 from flask import render_template, request, redirect, session, url_for
 from college.model import db, Student, Faculty, Notice, Result
+from sqlalchemy import desc
 
 @app.route("/studentprofile/<id>", methods = ["GET", "POST"])
 def student_profile(id):
@@ -8,10 +9,11 @@ def student_profile(id):
     logged = 'student' in session
     if logged:
         student = Student.query.get(id)
+        notices = Notice.query.order_by(desc(Notice.id)).limit(3).all()
         result = Result.query.filter_by(student_id=id).all()
 
         if not student:
             return "Student not found", 404
-        return render_template("student-profile.html", student=student, id=id, results=result, logged_in=True)
+        return render_template("student-profile.html", student=student, id=id, results=result, logged_in=True, notices=notices)
     return redirect(url_for("login"))
 
